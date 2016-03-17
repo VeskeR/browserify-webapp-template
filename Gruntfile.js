d0c87c758e2e3168459d7b281e7fe9932c46b18c
Andrey Bulat <andrii.bulat@gmail.com>
2016-03-17 23:11:07 +0200

    Added grunt basic layout

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = function (grunt) {
+  var packageConfig = grunt.file.readJSON('package.json');
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+  });
+
+  grunt.registerTask('default', []);
+}
