6e904114283ad51978dbb8a11e8cb989da42319b
Andrey Bulat <andrii.bulat@gmail.com>
2016-03-18 22:47:55 +0200

    Added watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,49 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+    watch: {
+      options: {
+        interrupt: true
+      },
+      bower: {
+        files: ['bower_components'],
+        tasks: [
 
+        ]
+      },
+      js: {
+        files: ['app/**/*.js'],
+        tasks: [
+
+        ]
+      },
+      sass: {
+        files: ['app/**/*.scss'],
+        tasks: [
+
+        ]
+      },
+      serve: {
+        options: {
+          livereload: 9000,
+          interrupt: false
+        },
+        files: [
+          '.tmp/**/*.{js,css,html,jpg,png,svg,gif,json}',
+          'app/**/*.html'
+        ]
+      },
+      dist: {
+        options: {
+          livereload: 9000,
+          interrupt: false
+        },
+        files: [
+          'dist/**/*.{js,css,html,jpg,png,svg,gif,json}',
+          'app/**/*.html'
+        ]
+      }
+    }
   });
 
   grunt.registerTask('default', []);
